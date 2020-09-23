7e55a713c3711f3fc402eb10e181f9551b3e3c81
Nicholas Fletcher <nick.fletcher@previousnext.com.au>
2020-09-23 16:15:22 +1000

    Update header

--- packages/header/src/Header.js
+++ packages/header/src/Header.js
@@ -2,12 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './header.css';
 
-const Header = ({ name, children }) => (
+const Header = ({ name, icon, link, children }) => (
   <header data-testid="header-test">
     <div className="wrapper">
       <div>
+        {icon}
+        <h1>
+          <a href={link}>
+            {name}
+          </a>
+        </h1>
+      </div>
+      <div>
         {children}
-        <h1>{name}</h1>
       </div>
     </div>
   </header>
@@ -15,10 +22,14 @@ const Header = ({ name, children }) => (
 
 Header.propTypes = {
   name: PropTypes.string.isRequired,
+  icon: PropTypes.node,
+  link: PropTypes.string,
   children: PropTypes.node,
 };
 
 Header.defaultProps = {
+  icon: null,
+  link: '/',
   children: null,
 };
 
--- packages/header/src/Header.stories.js
+++ packages/header/src/Header.stories.js
@@ -9,8 +9,30 @@ export default {
 
 const Template = args => <Header {...args} />;
 
+const TestMenu = () => (
+  <nav>
+    <ul>
+      <li>
+        <a href="/">Hey</a>
+      </li>
+      <li>
+        <a href="/">Ho</a>
+      </li>
+    </ul>
+  </nav>
+);
+
 export const Default = Template.bind({});
 Default.args = {
   name: 'Nick',
-  children: <img src={Logo} className="header__logo-icon" alt="Nick" />,
+  icon: <img src={Logo} className="header__logo-icon" alt="Nick" />,
+  link: '/',
+};
+
+export const Menu = Template.bind({});
+Menu.args = {
+  name: 'Nick',
+  icon: <img src={Logo} className="header__logo-icon" alt="Nick" />,
+  link: '/',
+  children: <TestMenu />,
 };
