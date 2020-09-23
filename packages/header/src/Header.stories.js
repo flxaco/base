03eec59738546b323482ca425f2d0064a6c78f15
Nicholas Fletcher <nick.fletcher@previousnext.com.au>
2020-09-23 14:31:18 +1000

    Update tests and start using Header

--- packages/header/src/Header.stories.js
+++ packages/header/src/Header.stories.js
@@ -9,10 +9,7 @@ export default {
 
 const Template = args => <Header {...args} />;
 
-export const LoggedIn = Template.bind({});
-LoggedIn.args = {
-  user: {},
+export const Default = Template.bind({});
+Default.args = {
+  name: 'Nick',
 };
-
-export const LoggedOut = Template.bind({});
-LoggedOut.args = {};
